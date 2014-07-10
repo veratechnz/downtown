1be7a6a1c4a61250bd5c68b9392c648a7b8bac53
veratech <jared@veratech.co.nz>
2014-07-10 17:39:02 +1200

    071014b

---
+++ inc/footer.inc.php
@@ -0,0 +1,52 @@
+		<!-- footer -->
+		<footer id="footerWrapper">
+			<section id="mainFooter">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="col-sm-4">
+							<div class="footerWidget">
+								<img src="images/foot-logo.png" alt="latest Little Neko news" id="footerLogo">
+							</div>
+							</div>
+							<div class="col-sm-6">
+								<p><a href="http://www.little-neko.com" title="Downtown Parking, Auckland">Downtown Parking</a> is a web design and development studio. We build responsive HTML5 and CSS3 templates.</p>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="footerWidget">
+
+								<h3 class="numargin">Downtown Parking</h3>
+								<address>
+									<p> <i class="icon-location"></i>&nbsp;77 Mass. Ave., E14/E15
+										Seattle<br> 
+										MA 02139-4307 USA <br>
+										<i class="icon-phone"></i>&nbsp;615.987.1234 
+										<i class="icon-mail-alt"></i>&nbsp;<a href="mailto:little@little-neko.com">little@little-neko.com</a> </p>
+									</address>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="footerWidget">
+									<h3 class="numargin">Company Info</h3>
+									<ul>
+									  	<li><a href="">Privacy Policy</a></li>
+									  	<li><a href="">Disclaimer</a></li>
+									  	<li><a href="">Terms & Conditions</a></li>
+									</ul>  
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section  id="footerRights">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<p>Copyright © 2014 <a href="http://www.little-neko.com" target="blank">Downtown Parking</a> / All rights reserved.</p>
+							</div>
+						</div>
+					</div>
+				</section>
+			</footer>
+			<!-- End footer -->
---
+++ privacy-policy.php
@@ -0,0 +1,80 @@
+<!-- head -->
+<?php include 'inc/head.inc.php';?>
+<!-- head -->
+<!-- Main navigation -->
+<?php include 'inc/nav.inc.php';?>
+<!-- End main navigation -->
+						</div>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<!-- header -->
+		<!-- ======================================= content ======================================= -->
+		<section id="page">
+			<header class="page-header mb30">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<h1>About</h1>
+						<ul class="breadcrumb visible-md visible-lg">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="template-menu-left.html">About</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</header>
+			<section id="content" class="mt30 pb30">
+				<div class="container">
+					<div class="row">
+					<!-- Sidebar -->
+					<?php include 'inc/sidebar.inc.php';?>
+					<!-- End Sidebar -->
+						<!-- page content-->
+						<section class="col-md-9">
+							<h2>Car Park Locations</h2>
+							<p class="mb30"> <strong> Quisque ut auctor lorem. Curabitur at lobortis nulla. Sed tincidunt, orci sit amet varius ornare, velit mi hendrerit purus, eget tempus ipsum felis vulputate metus. Praesent odio felis, vestibulum sit amet adipiscing quis, adipiscing </strong> </p>
+							<div class="row mb15">
+								<div class="col-md-4">
+									<div class="imgBorder"><img src="images/auckland-parking-a.jpg" alt="SEATTLE website template" class="img-responsive" /> </div>
+								</div>
+								<div class="col-md-8">
+									<h3>A local and family owner parking operator:</h3>
+									<p>Downtown Parking is the only 100% locally and family owned, private parking operator in New Zealand. Specialists in the Auckland market, we aim to deliver quality services and comprehensive car park management from small sites, to much larger facilities.</p><p> The broad and significant experience of our executive ensures innovation, ease of use, and customer service are always at the forefront of our business.</p>
+						</section>
+						<!--end page content-->
+					</div>
+				</div>
+			</section>
+		<!-- tabs & list -->
+		<?php include 'inc/info.inc.php';?>
+		<!-- tabs & list -->
+		<!-- services -->
+		<?php include 'inc/services.inc.php';?>
+		<!-- services -->
+		</section>
+		<!-- content -->
+		<!-- footer -->
+		<?php include 'inc/footer.inc.php';?>
+		<!-- End footer -->
+		</div>
+		<!-- global wrapper -->
+	<!-- End Document 
+	================================================== -->
+	<script type="text/javascript" src="js-plugin/respond/respond.min.js"></script>
+	<script type="text/javascript" src="js-plugin/jquery/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="js-plugin/jquery-ui/jquery-ui-1.8.23.custom.min.js"></script>
+	<!-- third party plugins  -->
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="js-plugin/easing/jquery.easing.1.3.js"></script>
+	<!-- carousel -->
+	<script type="text/javascript" src="js-plugin/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+	<!-- pop up -->
+	<script type="text/javascript" src="js-plugin/magnific-popup/jquery.magnific-popup.min.js"></script>
+	<!-- appear -->
+	<script type="text/javascript" src="js-plugin/appear/jquery.appear.js"></script>	
+	<!-- Custom  -->
+	<script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
